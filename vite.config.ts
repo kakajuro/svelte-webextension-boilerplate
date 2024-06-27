81903142a34cfb7324e38d57d5cc22332982ae9a
swishyy <famodavid1@gmail.com>
2024-06-27 11:38:51 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+// check if process.env is BUILD ALL
+// change build { outdir }, and build each plugin 
+// then script to zip all the files
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    webExtension({
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "manifest.json"],
+    }),
+  ],
+});
